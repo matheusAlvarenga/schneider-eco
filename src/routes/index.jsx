5068f69a537794ff878d41407a38b85883581371
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-06-07 13:27:49 -0300

    configure router

---
+++ src/routes/index.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/home';
+
+export function AppRouter() {
+  return (
+    <BrowserRouter basename='schneider-eco'>
+      <Routes>
+        <Route path="/" exact element={<Home></Home>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
